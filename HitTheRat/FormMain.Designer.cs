973fd87cbf46db04bd01adc5ac3dcf87a6587418
James Swineson <jamesswineson@gmail.com>
2016-07-29 13:17:34 +0800

    Initial Commit

---
+++ HitTheRat/FormMain.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace HitTheRat
+{
+	partial class FormMain
+	{
+		/// <summary>
+		/// 必要なデザイナ変数です。
+		/// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナで生成されたコード
+
+		/// <summary>
+		/// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディタで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btnquit = new System.Windows.Forms.Button();
+            this.gameTimer = new System.Windows.Forms.Timer();
+            this.lblscore = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.lblNotif = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(79, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 80);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(158, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 80);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(158, 139);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 80);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(79, 139);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 80);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(0, 139);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 80);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "button6";
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(158, 215);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 80);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "button7";
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(79, 215);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 80);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(0, 215);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 80);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "button9";
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // btnquit
+            // 
+            this.btnquit.Location = new System.Drawing.Point(198, 0);
+            this.btnquit.Name = "btnquit";
+            this.btnquit.Size = new System.Drawing.Size(40, 30);
+            this.btnquit.TabIndex = 9;
+            this.btnquit.Text = "Quit";
+            this.btnquit.Click += new System.EventHandler(this.btnquit_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 1000;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // lblscore
+            // 
+            this.lblscore.Location = new System.Drawing.Point(50, 0);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(142, 50);
+            this.lblscore.Text = "Score";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(0, 0);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(44, 30);
+            this.btnNewGame.TabIndex = 11;
+            this.btnNewGame.Text = "New";
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // lblNotif
+            // 
+            this.lblNotif.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Regular);
+            this.lblNotif.Location = new System.Drawing.Point(1, 31);
+            this.lblNotif.Name = "lblNotif";
+            this.lblNotif.Size = new System.Drawing.Size(236, 31);
+            this.lblNotif.Text = "Notification";
+            this.lblNotif.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(238, 295);
+            this.Controls.Add(this.lblNotif);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.btnquit);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Hit The Rat!";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnquit;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label lblNotif;
+
+    }
+}
+
---
+++ HitTheRat/FormMain.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace HitTheRat
+{
+	public partial class FormMain : Form
+	{
+        Button[] arrBoard;
+        Random r = new Random();
+        int currentScore = 0;
+        int highestScore = 0;
+        int lastSelectedTileIndex;
+        int contHitCount = 0;
+        bool clicked = true;
+        bool refreshedHighScore = false;
+        bool hasStarted = false;
+        int errorCount = 0;
+
+		public FormMain()
+		{
+			InitializeComponent();
+            arrBoard = new Button[] { button1, button2, button3, button6, button5, button4, button9, button8, button7 };
+		}
+
+        // UI logic
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            pause();
+            reset();
+            lblNotif.Text = "New Game";
+        }
+
+        private void btnquit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cleanBoard()
+        {
+            foreach (Button i in arrBoard)
+            {
+                i.Text = "";
+                i.BackColor = Color.White;
+            }
+        }
+
+        private void displayScore()
+        {
+            lblscore.Text = "Current: " + currentScore + "\nHigh: " + highestScore;
+            // lblNotif.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tileClicked(0, 0, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tileClicked(1, 0, 1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tileClicked(2, 0, 2);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            tileClicked(3, 1, 0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tileClicked(4, 1, 1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tileClicked(5, 1, 2);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            tileClicked(6, 2, 0);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            tileClicked(7, 2, 1);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            tileClicked(8, 2, 2);
+        }
+
+        // Game logic
+
+        private void tileClicked(int index, int x, int y)
+        {
+            if (hasStarted == false) return;
+            // MessageBox.Show("X=" + x + "Y=" + y);
+            clicked = true;
+            lblNotif.Text = "";
+            if (index == lastSelectedTileIndex)
+            {
+                currentScore += 100;
+                contHitCount += 1;
+                if (contHitCount > 2) lblNotif.Text = contHitCount + " hits!!!";
+                if (highestScore < currentScore)
+                {
+                    if (highestScore > 0 && refreshedHighScore == false) lblNotif.Text = "New High Score!";
+                    highestScore = currentScore;
+                    refreshedHighScore = true;
+                }
+            }
+            else
+            {
+                if (index == -1) lblNotif.Text = "Miss!";
+                else lblNotif.Text = "Error!";
+                contHitCount = 0;
+                errorCount += 1;
+                if (errorCount == 10)
+                {
+                    pause();
+                    reset();
+                    hasStarted = false;
+                    cleanBoard();
+                    return;
+                }
+            }
+            displayScore(); 
+            if (currentScore == 1000) gameTimer.Interval = 800;
+            if (currentScore == 2000) gameTimer.Interval = 600;
+            if (currentScore == 3000) gameTimer.Interval = 500;
+            if (currentScore == 4000) gameTimer.Interval = 400;
+            if (currentScore == 5000) gameTimer.Interval = 300;
+            if (currentScore == 6000) gameTimer.Interval = 200;
+            cleanBoard();
+        }
+
+        private void pause()
+        {
+            gameTimer.Enabled = false;
+            lblNotif.Text = "Paused";
+        }
+
+        private void unpause()
+        {
+            gameTimer.Enabled = true;
+            lblNotif.Text = "Start";
+            hasStarted = true;
+        }
+
+        private void reset()
+        {
+            currentScore = 0;
+            contHitCount = 0;
+            clicked = true;
+            refreshedHighScore = false;
+            errorCount = 0;
+            lastSelectedTileIndex = -2;
+            gameTimer.Interval = 1000;
+            cleanBoard();
+            displayScore();
+            hasStarted = false;
+            lblNotif.Text = "Game End";
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (hasStarted == false) {
+                cleanBoard();
+                return;
+            }
+            if (clicked == false) tileClicked(-1, 0, 0);
+            cleanBoard();
+            lastSelectedTileIndex = r.Next(arrBoard.Length);
+            arrBoard[lastSelectedTileIndex].Text = "Rat!";
+            arrBoard[lastSelectedTileIndex].BackColor = Color.Red;
+            clicked = false;
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            pause();
+            DialogResult ret = MessageBox.Show("Start new game?", "Hit The Rat!", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            if (ret == DialogResult.Yes)
+            {
+                reset();
+            }
+            unpause();
+        }
+	}
+}
